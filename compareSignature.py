00ff98105637777a8e1fd0161361ea36d0316cec
Hongen Lei <hongenlei99@gmail.com>
2023-12-01 14:30:44 -0800

    Refactor  to compare individual shots in videos, enhancing the granularity of video similarity analysis

--- compareSignature.py
+++ compareSignature.py
@@ -90,65 +90,101 @@ def detect_shot_boundaries(video_path, color_threshold, motion_threshold,
         current_hist = calculate_histogram(frame_downsampled)
         current_shot_histograms.append(current_hist)
 
-        hist_diff = cv2.compareHist(previous_hist, current_hist,
-                                    cv2.HISTCMP_BHATTACHARYYA)
-        motion_value = calculate_motion(previous_frame_gray, frame_gray)
+        # hist_diff = cv2.compareHist(previous_hist, current_hist,
+        #                             cv2.HISTCMP_BHATTACHARYYA)
+        # motion_value = calculate_motion(previous_frame_gray, frame_gray)
 
-        if hist_diff > color_threshold or motion_value > motion_threshold:
-            frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
-            avg_hist, motion_score = process_shot(shot_frames,
-                                                  current_shot_histograms)
-            histograms.append(avg_hist)
-            motion_scores.append(motion_score)
-            shot_boundaries.append((current_shot_start, frame_number - 1))
-            current_shot_start = frame_number
-            shot_frames, current_shot_histograms = [], []
+        # if hist_diff > color_threshold or motion_value > motion_threshold:
+        #     frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        #     avg_hist, motion_score = process_shot(shot_frames,
+        #                                           current_shot_histograms)
+        #     histograms.append(avg_hist)
+        #     motion_scores.append(motion_score)
+        #     shot_boundaries.append((current_shot_start, frame_number - 1))
+        #     current_shot_start = frame_number
+        #     shot_frames, current_shot_histograms = [], []
 
-        previous_frame_gray, previous_hist = frame_gray, current_hist
+        # previous_frame_gray, previous_hist = frame_gray, current_hist
 
     cap.release()
     return {"shotBoundaries": shot_boundaries, "histograms": histograms,
             "motionScores": motion_scores}
 
-def compare_signatures(query_signature, db_signatures):
-    """Compare video signatures to find the best match."""
-    match_scores = {}
+def compare_shot_signatures(query_signature, db_signatures):
+    """Compare shot signatures to find the most similar shots."""
+    shot_similarity_scores = []
 
-    for video_path, video_signature in db_signatures.items():
-        shot_scores = [
-            min(
-                [distance.euclidean(q_hist, db_hist) + abs(q_motion - db_motion)
-                 for db_shot_boundary, db_hist, db_motion in zip(
-                     video_signature['shotBoundaries'],
-                     video_signature['histograms'],
-                     video_signature['motionScores']
-                 )]
-            ) if query_signature['shotBoundaries'] else float('inf')
-            for q_shot_boundary, q_hist, q_motion in zip(
-                query_signature['shotBoundaries'],
-                query_signature['histograms'],
-                query_signature['motionScores']
-            )
-        ]
+    # Iterate through each shot in the query video
+    for q_shot_boundary, q_hist, q_motion in zip(
+            query_signature['shotBoundaries'],
+            query_signature['histograms'],
+            query_signature['motionScores']
+        ):
+        best_match_score = float('inf')
+        best_match_video = None
+        best_match_shot = None
 
-        avg_shot_score = (sum(shot_scores) / len(shot_scores)
-                          if shot_scores else float('inf'))
-        match_scores[video_path] = avg_shot_score
+        # Iterate through each video in the database
+        for video_path, video_signature in db_signatures.items():
+            # Iterate through each shot in the current database video
+            for db_shot_boundary, db_hist, db_motion in zip(
+                    video_signature['shotBoundaries'],
+                    video_signature['histograms'],
+                    video_signature['motionScores']
+                ):
+                # Calculate the similarity score for the current shot
+                score = distance.euclidean(q_hist, db_hist) + abs(q_motion - db_motion)
 
-    best_match_video = min(match_scores, key=match_scores.get)
-    return best_match_video, match_scores[best_match_video]
+                # Update the best match if the current score is lower
+                if score < best_match_score:
+                    best_match_score = score
+                    best_match_video = video_path
+                    best_match_shot = db_shot_boundary
+
+        # Append the best match for the current query shot
+        shot_similarity_scores.append({
+            "query_shot": q_shot_boundary,
+            "best_match_video": best_match_video,
+            "best_match_shot": best_match_shot,
+            "score": best_match_score
+        })
+
+    # Return the list of best matches for each shot in the query video
+    return shot_similarity_scores
+
+def frame_to_time(frame_number, fps=30):
+    """Convert a frame number to a timestamp in a video.
+
+    Args:
+    frame_number (int): The frame number to convert.
+    fps (int, optional): The frames per second of the video. Defaults to 30.
+
+    Returns:
+    str: A timestamp in the format 'HH:MM:SS'.
+    """
+    total_seconds = frame_number / fps
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 # Example usage:
 color_threshold = 0.5
 motion_threshold = 1.5e5
 downsample_percent = 50
 
-query_path = "Data/Queries/video2_1.mp4"
+query_path = "Data/Queries/video1_1.mp4"
 query_signature = detect_shot_boundaries(query_path, color_threshold,
                                          motion_threshold, downsample_percent)
 
 with open('signatures.json') as f:
     data = json.load(f)
 
-best_match, score = compare_signatures(query_signature, data)
-print(f"The best match is {best_match} with a score of {score}")
+# best_match, score = compare_shot_signatures(query_signature, data)
+# print(f"The best match is {best_match} with a score of {score}")
+
+shot_similarities = compare_shot_signatures(query_signature, data)
+for shot_match in shot_similarities:
+    query_shot = [frame_to_time(shot_match['query_shot'][0]), frame_to_time(shot_match['query_shot'][1])]
+    match_shot = [frame_to_time(shot_match['best_match_shot'][0]), frame_to_time(shot_match['best_match_shot'][1])]
+    print(f"Query shot: {query_shot} matches with shot: {match_shot} in video: {shot_match['best_match_video']} with a score of {shot_match['score']}")
