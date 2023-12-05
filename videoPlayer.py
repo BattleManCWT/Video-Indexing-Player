810f1a594abb241f2e61ae8eebe45daf64a9f544
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2023-12-04 20:34:59 -0800

    Update videoPlayer.py

--- videoPlayer.py
+++ videoPlayer.py
@@ -1,108 +1,117 @@
+# VideoPlayer.py
+
+import ffmpeg
 import cv2
+import numpy as np
 import tkinter as tk
-from tkinter import filedialog
+from PIL import Image, ImageTk
 import pygame
 
-
 class VideoPlayer:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Video Player")
+    def __init__(self, window, window_title, video_path, audio_path=None):
+        self.window = window
+        self.window.title(window_title)
+        self.video_path = video_path
+        self.audio_path = audio_path
 
-        self.cap = None
-        self.width, self.height = 0, 0
+        # Initialize Pygame for audio playback
+        if self.audio_path:
+            pygame.init()
+            pygame.mixer.init()
+            self.audio = pygame.mixer.Sound(audio_path)
 
-        self.audio_path = ""
+        # Open the video file with ffmpeg
+        self.process = (
+            ffmpeg
+            .input(video_path)
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .run_async(pipe_stdout=True)
+        )
 
-        self.canvas = tk.Canvas(master)
+        # Get video properties
+        self.vid = cv2.VideoCapture(video_path)
+        self.width = int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.canvas = tk.Canvas(window, width=self.width, height=self.height)
         self.canvas.pack()
 
-        self.btn_reset = tk.Button(master, text="Reset", command=self.reset_video)
-        self.btn_play = tk.Button(master, text="Play", command=self.play_video)
-        self.btn_pause = tk.Button(master, text="Pause", command=self.pause_video)
-        self.btn_open = tk.Button(master, text="Open Video", command=self.open_video)
-        self.btn_reset.pack(side=tk.LEFT)
+        # Play button
+        self.btn_play = tk.Button(window, text="Play", command=self.play)
         self.btn_play.pack(side=tk.LEFT)
+
+        # Pause button
+        self.btn_pause = tk.Button(window, text="Pause", command=self.pause)
         self.btn_pause.pack(side=tk.LEFT)
-        self.btn_open.pack(side=tk.LEFT)
 
-        self.is_playing = False
-        self.update()
+        # Reset button
+        self.btn_reset = tk.Button(window, text="Reset", command=self.reset)
+        self.btn_reset.pack(side=tk.LEFT)
 
-    def reset_video(self):
-        if self.cap:
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            self.update_canvas()
+        self.running = False
 
+    def play(self):
+        if not self.running:
+            self.running = True
             if self.audio_path:
-                pygame.mixer.music.stop()  # Stop the current playback
-                pygame.mixer.music.load(self.audio_path)  # Reload the audio file
-                pygame.mixer.music.play()  # Start playing the audio from the beginning
+                pygame.mixer.unpause()  # Unpause the audio if it was paused
+                if not pygame.mixer.get_busy():  # Play only if the audio is not already playing
+                    pygame.mixer.Sound.play(self.audio)
+            self.read_frame()
 
-                if not self.is_playing:
-                    pygame.mixer.music.pause()  # Pause the audio if the video is paused
+    def pause(self):
+        self.running = False
+        if self.audio_path:
+            pygame.mixer.pause()  # Pause the audio
 
-    def play_video(self):
-        if not pygame.mixer.get_init():
-            pygame.mixer.init()
-        self.is_playing = True
-        pygame.mixer.music.unpause()
-        self.play_audio()
+    def reset(self):
+        self.running = False
+        if self.audio_path:
+            pygame.mixer.Sound.stop(self.audio)
 
-    def pause_video(self):
-        self.is_playing = False
-        pygame.mixer.music.pause()
+        # Kill the existing FFmpeg process
+        if self.process is not None:
+            self.process.kill()
 
-    def open_video(self):
-        try:
-            video_path = filedialog.askopenfilename(title="Select Video", filetypes=[
-                ("All files", "*.*")])  # Allow all file types for testing
-            if video_path:
-                self.cap = cv2.VideoCapture(video_path)
-                if not self.cap.isOpened():  # Check if the video capture is successfully opened
-                    raise IOError("Cannot open video file")
-                self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                self.audio_path = ""  # Reset the audio path when a new video is opened
-                self.update_canvas()
+        # Start a new FFmpeg process
+        self.process = (
+            ffmpeg
+            .input(self.video_path)
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .run_async(pipe_stdout=True)
+        )
 
-                # Optional: Ask for audio file only if the video is successfully opened
-                audio_path = filedialog.askopenfilename(title="Select Audio",
-                                                        filetypes=[("Audio files", "*.wav"), ("All files", "*.*")])
-                if audio_path:
-                    self.audio_path = audio_path
+        # Immediately read and display the first frame
+        in_bytes = self.process.stdout.read(self.width * self.height * 3)
+        if in_bytes:
+            frame = (
+                np
+                .frombuffer(in_bytes, np.uint8)
+                .reshape([self.height, self.width, 3])
+            )
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
-        except Exception as e:
-            print(f"Error opening video: {e}")
-            # Optionally, you can show an error message on the GUI
+    def read_frame(self):
+        if self.running:
+            in_bytes = self.process.stdout.read(self.width * self.height * 3)
+            if not in_bytes:
+                self.running = False
+                return
+            frame = (
+                np
+                .frombuffer(in_bytes, np.uint8)
+                .reshape([self.height, self.width, 3])
+            )
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+            self.window.after(15, self.read_frame)
 
-    def play_audio(self):
-        if not pygame.mixer.get_init():
-            pygame.mixer.init()
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.load(self.audio_path)
-            pygame.mixer.music.play(start=pygame.mixer.music.get_pos())
+    def __del__(self):
+        if self.process is not None:
+            self.process.kill()
+        if self.vid.isOpened():
+            self.vid.release()
+        if self.audio_path:
+            pygame.quit()
 
-    def update_canvas(self):
-        if self.cap:
-            ret, frame = self.cap.read()
-            if ret:
-                # 确保从BGR转换到RGB
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-                img = tk.PhotoImage(data=self.convert_frame(frame))
-                self.canvas.config(width=self.width, height=self.height)
-                self.canvas.img_tk = img
-                self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
-
-    def convert_frame(self, frame):
-        return cv2.imencode('.PPM', frame)[1].tobytes()
-
-    def update(self):
-        if self.is_playing:
-            self.update_canvas()
-
-        self.master.after(30, self.update)
-
-    def run(self):
-        self.master.mainloop()
