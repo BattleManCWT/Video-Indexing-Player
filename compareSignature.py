8d572588e56aa609ef842290c03c75aca2a4d241
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2023-12-05 18:35:48 -0800

    Update compareSignature.py

--- compareSignature.py
+++ compareSignature.py
@@ -113,9 +113,63 @@ def find_offset(within_file, find_file):
 
     return offset
 
+# def extract_frames(video_path: str, interval: float) -> List[np.ndarray]:
+#     """
+#     Extract frames from a video at a specified interval.
+#
+#     :param video_path: Path to the video file.
+#     :param interval: Interval at which to extract frames (in seconds).
+#     :return: List of extracted frames.
+#     """
+#     cap = cv2.VideoCapture(video_path)
+#     fps = cap.get(cv2.CAP_PROP_FPS)
+#     frames = []
+#
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+#         frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+#         if frame_number % int(fps * interval) == 0:
+#             frames.append(frame)
+#
+#     cap.release()
+#     return frames
+#
+# def find_matching_frame(query_frames: List[np.ndarray], db_frames: List[np.ndarray]) -> Tuple[int, int, float]:
+#     """
+#     Find the best matching frame from the database for a query video.
+#
+#     :param query_frames: List of frames from the query video.
+#     :param db_frames: List of frames from the database video.
+#     :return: Tuple of (query frame index, db frame index, similarity score).
+#     """
+#     best_match = (None, None, float('inf'))
+#     for i, query_frame in enumerate(query_frames):
+#         query_hist = calculate_histogram(query_frame)
+#         for j, db_frame in enumerate(db_frames):
+#             db_hist = calculate_histogram(db_frame)
+#             distance = cv2.compareHist(query_hist, db_hist, cv2.HISTCMP_CHISQR)
+#             if distance < best_match[2]:
+#                 best_match = (i, j, distance)
+#
+#     return best_match
+
+def calculate_frame_number(offset_seconds: float, fps: int) -> int:
+    """
+    Calculate the frame number based on the offset time and FPS.
+
+    :param offset_seconds: Offset time in seconds.
+    :param fps: Frames per second of the video.
+    :return: Frame number corresponding to the offset time.
+    """
+    frame_number = int(offset_seconds * fps)
+    return frame_number
+
+
 def main():
     # query_path = "Data/Queries"
-    query_video_path = "Data/Queries/video9_1.mp4"
+    query_video_path = "Queries/video9_1.mp4"
     query_video_name = query_video_path.split("/")[-1][:-4]
 
     downsample_percent = 50
@@ -131,11 +185,27 @@ def main():
     closest_video, similarity_score = compare_video_signatures(query_histogram, db_signatures)
     print(f"The most similar video to {query_video_path} is {Path(closest_video).name} with a similarity score of {similarity_score}")
     closest_video_name = closest_video.split("/")[-1][:-4]
-    offset = find_offset( f"Data/Audios/{closest_video_name}.wav", f"Data/Queries/audios/{query_video_name}.wav")
+    offset = find_offset( f"Audios/{closest_video_name}.wav", f"Queries/audios/{query_video_name}.wav")
     offset_minutes = offset // 60
     offset_remainder_seconds = offset % 60
     print(f"Offset: {offset}s" )
     print(f"Offset: {int(offset_minutes)}m {round(offset_remainder_seconds, 2)}s")
+
+    offset = find_offset(f"Audios/{closest_video_name}.wav", f"Queries/audios/{query_video_name}.wav")
+
+    # Assuming you have the FPS value for the database video
+    fps = 30  # Replace this with the actual FPS of your database video
+
+    frame_number = calculate_frame_number(offset, fps)
+    print(f"Frame number corresponding to the offset: {frame_number}")
+
+    # query_frames = extract_frames("Queries/video9_1.mp4", 1)  # Extract frames every 1 second
+    # db_video_path = closest_video  # This is from your existing code
+    # db_frames = extract_frames(f"Videos/{closest_video_name}.mp4", 1)
+    #
+    # query_frame_index, db_frame_index, similarity_score = find_matching_frame(query_frames, db_frames)
+    # print(f"Best match at query frame {query_frame_index} and database frame {db_frame_index} with similarity score {similarity_score}")
+
 
 if __name__ == "__main__":
     main()
