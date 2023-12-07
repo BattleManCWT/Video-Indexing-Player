d2c62a31dea2edf3f3a69d8fab28322646d2f2a7
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2023-12-06 17:50:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from Player import VideoPlayer
 from compareSignature import create_query_signature, compare_video_signatures, find_offset, calculate_frame_number
 from rgbCompare import locate_exact_frame
 
+
 def main():
-    query_video_path = "Data/Queries/videos/video1_1.mp4"
+    query_video_path = "/Volumes/T7 Shield/Queries/MP4/video7_1.mp4"
+    # query_video_path = "Test/video15_3.mp4"
     query_video_name = query_video_path.split("/")[-1][:-4]
 
     downsample_percent = 50
@@ -16,9 +18,11 @@ def main():
 
     query_histogram = create_query_signature(query_video_path, color_threshold, downsample_percent)
     closest_video, similarity_score = compare_video_signatures(query_histogram, db_signatures)
-    print(f"The most similar video to {query_video_path} is {Path(closest_video).name} with a similarity score of {similarity_score}")
+    print(
+        f"The most similar video to {query_video_path} is {Path(closest_video).name} with a similarity score of {similarity_score}")
     closest_video_name = closest_video.split("/")[-1][:-4]
-    offset = find_offset(f"Data/Audios/{closest_video_name}.wav", f"Data/Queries/audios/{query_video_name}.wav")
+    offset = find_offset(f"/Volumes/T7 Shield/Videos/Audios/{closest_video_name}.wav", f"/Volumes/T7 Shield/Queries/Audios/{query_video_name}.wav")
+    # offset = find_offset(f"/Volumes/T7 Shield/Videos/Audios/{closest_video_name}.wav", f"Test/{query_video_name}.wav")
     offset_minutes = offset // 60
     offset_remainder_seconds = offset % 60
     print(f"Offset: {offset}s")
@@ -26,15 +30,20 @@ def main():
 
     # Calculate the specific frame
     approximate_location = calculate_frame_number(offset, 30)
-    frame_to_start = locate_exact_frame(f"Data/Queries/videos/{query_video_name}.rgb", f"Test_rgb/{closest_video_name}.rgb", approximate_location, 5)
+    frame_to_start = locate_exact_frame(f"/Volumes/T7 Shield/Queries/RGB_Files/{query_video_name}.rgb",
+                                        f"/Volumes/T7 Shield/Videos/rgb_files/{closest_video_name}.rgb", approximate_location, 5)
+    # frame_to_start = locate_exact_frame(f"Test_rgb/{query_video_name}.rgb",
+    #                                     f"/Volumes/T7 Shield/Videos/rgb_files/{closest_video_name}.rgb", approximate_location, 5)
     print(f"Start frame: {frame_to_start}")
     # Initialize the video player
     root = tk.Tk()
     root.title(f"{closest_video_name}")
-    player = VideoPlayer(root, f"Test_rgb/{closest_video_name}.rgb", f"Data/Audios/{closest_video_name}.wav", frame_to_start)
+    player = VideoPlayer(root, f"/Volumes/T7 Shield/Videos/rgb_files/{closest_video_name}.rgb", f"/Volumes/T7 Shield/Videos/Audios/{closest_video_name}.wav",
+                         frame_to_start)
     player.play_from_frame(frame_to_start)
     player.pause_video()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
